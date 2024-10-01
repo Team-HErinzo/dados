0c7256a318575c0fd8f09bb79190342587a2910f
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-10-01 19:31:15 -0300

    .

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ async function visualizarDados(){
     const res = await fetch(url)
     const dados = await res.json()
     regiao = 0
-     resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+    resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
     console.log(resultado_dados, resultado_regiao)
   }
