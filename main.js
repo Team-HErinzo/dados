4e5d39fe23cfb6d9ead0d9a0a7af3556d02e7422
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-10-02 09:36:55 -0300

    ...

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ async function visualizarDados(){
     const res = await fetch(url)
     const dados = await res.json()
     regiao = 0
+    const tipo_dados = dados[0] ["variavel"]
+    console.log(tipo_dados)
     resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
     console.log(resultado_dados, resultado_regiao)
   }
   visualizarDados()
   
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos--container__texto')
+paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
+container.appendChild(paragrafo)
