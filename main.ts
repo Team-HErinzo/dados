886749fd18098a2d83d4fa197186b0f21494e8a0
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-10-02 22:02:02 -0300

    .

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ async function visualizarDados() {
 }
 visualizarDados();
 
-const paragrafo = document.createElement('p')
-paragrafo.classList.add('graficos--container__texto')
-paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
-container.appendChild(paragrafo)
+// const paragrafo = document.createElement('p')
+// paragrafo.classList.add('graficos--container__texto')
+// paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
+// container.appendChild(paragrafo)
