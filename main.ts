c74bbde200a6f8bb569e621e8d30f11dfc6e8686
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-10-08 08:03:28 -0300

    ;

--- main.ts
+++ main.ts
@@ -13,7 +13,3 @@ async function visualizarDados() {
 }
 visualizarDados();
 
-// const paragrafo = document.createElement('p')
-// paragrafo.classList.add('graficos--container__texto')
-// paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
-// container.appendChild(paragrafo)
